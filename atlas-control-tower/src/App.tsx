3815050ebdec36eec0d51ecab3e7b604a15fd23e
byDenoso <denosooo2@gmail.com>
2026-09-11 06:42:44 -0300

    feat: add graphs drill-down routes

--- atlas-control-tower/src/App.tsx
+++ atlas-control-tower/src/App.tsx
@@ -6,6 +6,9 @@ const OverviewPage = lazy(()=>import('./pages/OverviewPage'));
 const UniversesPage = lazy(()=>import('./pages/UniversesPage'));
 const UniversePage = lazy(()=>import('./pages/UniversePage'));
 const SubdomainPage = lazy(()=>import('./pages/SubdomainPage'));
+const GraphsPage = lazy(()=>import('./pages/GraphsPage'));
+const GraphDomainPage = lazy(()=>import('./pages/GraphDomainPage'));
+const GraphDetailPage = lazy(()=>import('./pages/GraphDetailPage'));
 const LearningPage = lazy(()=>import('./pages/LearningPage'));
 const OperationsPage = lazy(()=>import('./pages/OperationsPage'));
 const ProvenancePage = lazy(()=>import('./pages/ProvenancePage'));
@@ -19,6 +22,9 @@ export default function App(){
           <Route path="/universes" element={<UniversesPage/>}/>
           <Route path="/universes/:universeId" element={<UniversePage/>}/>
           <Route path="/universes/:universeId/:subdomainId" element={<SubdomainPage/>}/>
+          <Route path="/graphs" element={<GraphsPage/>}/>
+          <Route path="/graphs/:domainId" element={<GraphDomainPage/>}/>
+          <Route path="/graphs/:domainId/:subgraphId" element={<GraphDetailPage/>}/>
           <Route path="/learning" element={<LearningPage/>}/>
           <Route path="/operations" element={<OperationsPage/>}/>
           <Route path="/provenance" element={<ProvenancePage/>}/>
