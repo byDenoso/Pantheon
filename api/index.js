f97faf156967c00011010089a8b2dfea086d386e
byDenoso <denosooo2@gmail.com>
2026-09-16 23:48:38 -0300

    fix(deploy): make Vercel API entrypoint statically traceable

--- api/index.js
+++ api/index.js
@@ -1,9 +1,3 @@
-import {pathToFileURL} from 'node:url';
+import handler from '../nexo-one/server/handler.mjs';
 
-const handlerUrl=pathToFileURL(`${process.cwd()}/nexo-one/server/handler.mjs`).href;
-const handlerPromise=import(handlerUrl).then(module=>module.default);
-
-export default async function handler(req,res){
-  const runtimeHandler=await handlerPromise;
-  return runtimeHandler(req,res);
-}
+export default handler;
